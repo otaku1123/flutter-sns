ef1977407529b90cdf25205eb40711c76dbfffd3
Taku Ogawa <ogawtaku@gmail.com>
2022-06-10 03:08:12 +0900

    BottomNavigationを実装 （投稿用ページへの画面遷移を実装）

--- lib/view/time_line/time_line_page.dart
+++ lib/view/time_line/time_line_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:sns_app/model/account.dart';
 import 'package:sns_app/model/post.dart';
-import 'package:sns_app/view/time_line/post_page.dart';
 
 class TimeLinePage extends StatefulWidget {
   TimeLinePage({Key? key}) : super(key: key);
@@ -97,12 +96,6 @@ class _TimeLinePageState extends State<TimeLinePage> {
             ),
           );
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => PostPage()));
-        },
-        child: Icon(Icons.chat_bubble_outline),
       ),
     );
   }
