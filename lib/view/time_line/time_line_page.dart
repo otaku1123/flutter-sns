f69ff75d8cb00ad48a9c691e29fddabbb7c4f0a5
Taku Ogawa <ogawtaku@gmail.com>
2022-06-10 00:41:32 +0900

    タイムラインのUIを作成

--- lib/view/time_line/time_line_page.dart
+++ lib/view/time_line/time_line_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:sns_app/model/account.dart';
+import 'package:sns_app/model/post.dart';
 
 class TimeLinePage extends StatefulWidget {
   TimeLinePage({Key? key}) : super(key: key);
@@ -9,20 +11,92 @@ class TimeLinePage extends StatefulWidget {
 }
 
 class _TimeLinePageState extends State<TimeLinePage> {
-  Account myAccont = Account(
-    id: '1',
-    name: 'Flutterラボ',
-    selfIntroduction: 'こんばんは',
-    userId: 'flutter_lab',
-    imagePath: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fyt3.ggpht.com%2FngVd2-zv5o3pGUCfiVdZXCHhnq_g1Lo1Y8DbrmB9O8G7DG0IWUQJgsacqsI_LRvZE8JTsbQIuQ%3Ds900-c-k-c0x00ffffff-no-rj&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fchannel%2FUCrzA_ub-JcI_8daABLfOg-g&tbnid=JXDqsRXdjm9VcM&vet=12ahUKEwjoquCWq574AhWTRvUHHeyVAgwQMygAegUIARClAQ..i&docid=oXCS6nWt6AKvNM&w=900&h=900&q=flutter%E3%83%A9%E3%83%9C&ved=2ahUKEwjoquCWq574AhWTRvUHHeyVAgwQMygAegUIARClAQ',
-    createdTime: DateTime.now(),
-    updatedTime: DateTime.now()
-  );
+  Account myAccount = Account(
+      id: '1',
+      name: 'Flutterラボ',
+      selfIntroduction: 'こんばんは',
+      userId: 'flutter_lab',
+      imagePath:
+          'https://yt3.ggpht.com/ngVd2-zv5o3pGUCfiVdZXCHhnq_g1Lo1Y8DbrmB9O8G7DG0IWUQJgsacqsI_LRvZE8JTsbQIuQ=s900-c-k-c0x00ffffff-no-rj',
+      createdTime: DateTime.now(),
+      updatedTime: DateTime.now());
+
+  List<Post> postList = [
+    Post(
+        id: '1',
+        content: '初めまして',
+        postAccountId: '1',
+        createdTime: DateTime.now()),
+    Post(
+        id: '2',
+        content: '初めまして2回',
+        postAccountId: '1',
+        createdTime: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('タイムライン'),),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'タイムライン',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Theme.of(context).canvasColor,
+        elevation: 2,
+      ),
+      body: ListView.builder(
+        itemCount: postList.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              border: index == 0 ? Border(
+                top: BorderSide(color: Colors.grey, width: 0),
+                bottom: BorderSide(color: Colors.grey, width:0),
+              ): Border(bottom: BorderSide(color: Colors.grey, width: 0))
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 22,
+                  foregroundImage: NetworkImage(myAccount.imagePath),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  myAccount.name,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  '@${myAccount.userId}',
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                            Text(DateFormat('M/d/yy')
+                                .format(postList[index].createdTime!))
+                          ],
+                        ),
+                        Text(postList[index].content),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
