561aef116e9aff97e25ea90d8563367098b21016
Taku Ogawa <ogawtaku@gmail.com>
2022-06-12 14:11:14 +0900

    アカウントページに投稿した情報を表示

--- lib/view/account/account_page.dart
+++ lib/view/account/account_page.dart
@@ -4,10 +4,12 @@ import 'package:intl/intl.dart';
 import 'package:sns_app/model/account.dart';
 import 'package:sns_app/model/post.dart';
 import 'package:sns_app/utils/authentication.dart';
+import 'package:sns_app/utils/firestore/posts.dart';
+import 'package:sns_app/utils/firestore/users.dart';
 import 'package:sns_app/view/account/edit_account_page.dart';
 
 class AccountPage extends StatefulWidget {
-  const AccountPage({ Key? key }) : super(key: key);
+  const AccountPage({Key? key}) : super(key: key);
 
   @override
   State<AccountPage> createState() => _AccountPageState();
@@ -16,153 +18,190 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   Account myAccount = Authentication.myAccount!;
 
-  List<Post> postList = [
-    Post(
-        id: '1',
-        content: '初めまして',
-        postAccountId: '1',
-        createdTime: Timestamp.now()),
-    Post(
-        id: '2',
-        content: '初めまして2回',
-        postAccountId: '1',
-        createdTime: Timestamp.now()),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(right: 15, left: 15, top: 20),
-                  height: 200,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
+        body: SafeArea(
+      child: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(right: 15, left: 15, top: 20),
+                height: 200,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
                             CircleAvatar(
                               radius: 32,
-                              foregroundImage: NetworkImage(myAccount.imagePath),
+                              foregroundImage:
+                                  NetworkImage(myAccount.imagePath),
                             ),
-                            SizedBox(width: 10,height: 20,),
+                            SizedBox(
+                              width: 10,
+                              height: 20,
+                            ),
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
-                                    myAccount.name,
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                                  myAccount.name,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                                Text(
-                                    '@${myAccount.userId}',
-                                    style: TextStyle(color: Colors.grey)
-                                ),
+                                Text('@${myAccount.userId}',
+                                    style: TextStyle(color: Colors.grey)),
                               ],
                             )
                           ],
-                          ),
-                          ElevatedButton(
-                            onPressed: () async {
-                              var result = await Navigator.push(context, MaterialPageRoute(builder: (context)=> EditAccountPage()));
-                              if (result == true) {
-                                setState(() {
-                                  myAccount = Authentication.myAccount!;
-                                });
-                              }
-                            },
-                            child: Text('編集'),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text(myAccount.selfIntroduction),
-                    ],
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.blue, width: 3
-                      )
-                    )
-                  ),
-                  child: Text('投稿',
-                  style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Expanded(
-                    child: ListView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: postList.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          decoration: BoxDecoration(
-                              border: index == 0 ? Border(
-                                top: BorderSide(color: Colors.grey, width: 0),
-                                bottom: BorderSide(color: Colors.grey, width:0),
-                              ): Border(bottom: BorderSide(color: Colors.grey, width: 0))
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                          child: Row(
-                            children: [
-                              CircleAvatar(
-                                radius: 22,
-                                foregroundImage: NetworkImage(myAccount.imagePath),
-                              ),
-                              Expanded(
-                                child: Container(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Row(
-                                            children: [
-                                              Text(
-                                                myAccount.name,
-                                                style: TextStyle(fontWeight: FontWeight.bold),
-                                              ),
-                                              Text(
-                                                '@${myAccount.userId}',
-                                                style: TextStyle(color: Colors.grey),
-                                              ),
-                                            ],
-                                          ),
-                                          Text(DateFormat('M/d/yy')
-                                              .format(postList[index].createdTime!.toDate()))
-                                        ],
-                                      ),
-                                      Text(postList[index].content),
-                                    ],
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-
-                      },
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            var result = await Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => EditAccountPage()));
+                            if (result == true) {
+                              setState(() {
+                                myAccount = Authentication.myAccount!;
+                              });
+                            }
+                          },
+                          child: Text('編集'),
+                        )
+                      ],
                     ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(myAccount.selfIntroduction),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(color: Colors.blue, width: 3))),
+                child: Text(
+                  '投稿',
+                  style: TextStyle(
+                      color: Colors.blue, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: UserFirestore.users
+                      .doc(myAccount.id)
+                      .collection('my_posts')
+                      .orderBy('created_time', descending: true)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      List<String> myPostIds =
+                          List.generate(snapshot.data!.docs.length, (index) {
+                        return snapshot.data!.docs[index].id;
+                      });
+
+                      return FutureBuilder<List<Post>?>(
+                          future: PostFirestore.getPostsFromIds(myPostIds),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return ListView.builder(
+                                physics: NeverScrollableScrollPhysics(),
+                                itemCount: snapshot.data!.length,
+                                itemBuilder: (context, index) {
+                                  Post post = snapshot.data![index];
+                                  return Container(
+                                    decoration: BoxDecoration(
+                                        border: index == 0
+                                            ? Border(
+                                                top: BorderSide(
+                                                    color: Colors.grey,
+                                                    width: 0),
+                                                bottom: BorderSide(
+                                                    color: Colors.grey,
+                                                    width: 0),
+                                              )
+                                            : Border(
+                                                bottom: BorderSide(
+                                                    color: Colors.grey,
+                                                    width: 0))),
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 10, vertical: 15),
+                                    child: Row(
+                                      children: [
+                                        CircleAvatar(
+                                          radius: 22,
+                                          foregroundImage:
+                                              NetworkImage(myAccount.imagePath),
+                                        ),
+                                        Expanded(
+                                          child: Container(
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    Row(
+                                                      children: [
+                                                        Text(
+                                                          myAccount.name,
+                                                          style: TextStyle(
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .bold),
+                                                        ),
+                                                        Text(
+                                                          '@${myAccount.userId}',
+                                                          style: TextStyle(
+                                                              color:
+                                                                  Colors.grey),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                    Text(DateFormat('M/d/yy')
+                                                        .format(post
+                                                            .createdTime!
+                                                            .toDate()))
+                                                  ],
+                                                ),
+                                                Text(post.content),
+                                              ],
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  );
+                                },
+                              );
+                            } else {
+                              return Container();
+                            }
+                          });
+                    } else {
+                      return Container();
+                    }
+                  },
+                ),
+              ),
+            ],
           ),
         ),
-      )
-    );
+      ),
+    ));
   }
 }
